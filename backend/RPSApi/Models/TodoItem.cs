d671899a3f952cdd3c50662edc9f3955cb35c639
Andrew Way <andrew@wayfamily.net>
2021-04-30 11:21:53 -0700

    save changes

---
+++ backend/RPSApi/Models/TodoItem.cs
@@ -0,0 +1,10 @@
+namespace RPSApi.Models
+{
+    public class TodoItem
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public bool IsComplete { get; set; }
+        public string Secret { get; set; }
+    }
+}
